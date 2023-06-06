0186d710f4d84784123365782642e0a18e40e16b
Ruslan <ru@skylinehealthgroup.com>
2023-06-06 20:45:11 +0300

    create wpsync-webspark plagyn

---
+++ wpsync-webspark.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: wpsync-webspark
+Description: updates the product database for woocommerce.
+Version:  1.0
+Author: Ruslan Toloshnyi
+*/
+
+/*  Copyright 2023  Ruslan Toloshnyi  (email: ruslantoloshnyi@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
